e97b95bfa3574c7acee8d1fbdc4a3d5c49a63dc7
Michael Katzenellenbogen <michael@kazenonline.com>
2020-08-27 01:42:44 +0000

    adding build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+tag=$(git describe --tags --abbrev=0)
+repo=docker.io/emsixteeen
+image=k8s-audit-webhook
+image_tag="${repo}/${image}:${tag}"
+
+echo "Building ${image_tag}"
+docker build -f Dockerfile -t ${image_tag} .
+
+echo "Pusing ${image_tag}"
+docker push ${image_tag}
